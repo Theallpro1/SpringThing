13e2a5df9994751f3eee6b6d7cf8863fcb66b476
Matthew Patkowski <38364214+Theallpro1@users.noreply.github.com>
2022-09-06 13:31:40 -0400

    Create Link.py

---
+++ Link.py
@@ -0,0 +1,13 @@
+class Link():
+	def __init__(self, pos, vel):
+		self.pos = pos
+		self.vel = vel
+	
+	def get_pos(self):
+		return self.pos
+	def set_pos(self, p):
+		self.pos = p
+	def set_vel(self,v):
+		self.vel = v 
+	def get_vel(self):
+		return self.vel
